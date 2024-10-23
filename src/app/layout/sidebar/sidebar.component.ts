28e283c073698095b1f2276467a84c6d4394d392
Rodrigo Molina <rodrimr2008@gmail.com>
2024-10-22 22:53:24 -0300

    Making navigation work with routermodule

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-
+import { RouterModule } from '@angular/router';
 
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatToolbar],
+  imports: [MatButtonModule, MatMenuModule, MatToolbar, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
