7fb9e8fd66e752efda1d1eeffe560037d89cad20
Rodrigo Molina <rodrimr2008@gmail.com>
2024-10-22 22:49:47 -0300

    Added routing into alumni list and courses list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AlumniListComponent } from './dashboard/alumni-list/alumni-list.component';
+import { CourseListComponent } from './dashboard/course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'alumni', component: AlumniListComponent },
+  { path: 'courses', component: CourseListComponent },
+  { path: '', redirectTo: '/alumni', pathMatch: 'full' },
+  { path: '**', redirectTo: '/alumni' }
+];
